6f1e1bfe5b11f6f8712d96c6bec20e5db7caff46
momoislam8849 <shampa15-8849@diu.edu.bd>
2020-09-09 12:04:55 +0600

    Adding new files

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('contact.html', views.contact, name="contact"),
+
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
 	return render(request,'home.html',{})
 
+
+def contact(request):
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+		return render(request,'contact.html',{'message_name':message_name})
+
+	else:
+		return render(request,'contact.html',{})
+
+
