56474bc00b2ba1fcaf1d451227f27485b160e5d4
momoislam8849 <shampa15-8849@diu.edu.bd>
2020-09-18 12:01:52 +0600

    initial

--- dentist/settings.py
+++ dentist/settings.py
@@ -22,7 +22,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env.str('secret_key')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
